ed03055d21590569e9a954495e451cafd1475773
EvgeniiK <inso.jk@gmail.com>
2024-07-20 14:04:55 +0700

    init

---
+++ src/components/Main/Modules/Statistic/Statistic.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="statistic">
+
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/components/Main/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Simon Game</h1>
+
+  <div class="main__wrapper">
+    <game-block />
+    <statistic />
+  </div>
+</template>
+
+<script setup lang="ts">
+import './styles.scss'
+import GameBlock from './Modules/GameBlock/GameBlock.vue'
+import Statistic from './Modules/Statistic/Statistic.vue';
+</script>
